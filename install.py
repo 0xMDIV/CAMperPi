1c6de9066df742fd43ff4495803da8da779adfa3
0xMDIV <0xMDIV@gmail.com>
2019-02-26 13:54:58 +0100

    better install system

--- install.py
+++ install.py
@@ -9,74 +9,120 @@ err1 = 'i2c installation failed!'
 err2 = 'python smbus installation failed!'
 err3 = 'wiring pi installation failed!'
 err4 = 'camera setup failed'
-
-try:
-    print('installing needed stuff...')
-    print('upgrade Pi...')
-    os.system('sudo dpkg --configure -a')
-    # check for new updates and install when needed
-    os.system('sudo apt-get update -y && sudo apt-get upgrade -y')
-    os.system('clear')
-except:
-    print(err0, sys.exc_info()[0])
-    failcounter += 1
-
-try:
-    print('pi sucessfull updated, continue...')
-    print('installing i2c tools...')
-    os.system('sudo apt-get install i2c-tools -y')
-    os.system('sudo adduser pi i2c')
-    print('tesing if i2c is working')
-    os.system('sudo i2cdetect -y 1')
-    time.sleep(2)
-    os.system('clear')
-except:
-    print(err1, sys.exc_info()[0])
-    failcounter += 1
-
-try:
-    print('finished installing i2c tools continue...')
-    print('install smbus...')
-    os.system('sudo apt-get install python-smbus -y')
-    os.system('clear')
-except:
-    print(err2, sys.exc_info()[0])
-    failcounter += 1
-
-try:
-    print('installing wiringPi ...')
-    os.system('sudo apt-get install git git-core -y')
-    os.system('sudo pip install wiringpi')
-    os.system('clear')
-    print('wiringPi installation succesfull')
-except:
-    print(err3, sys.exc_info()[0])
-    failcounter += 1
-
-try:
-    print('setup the Camera...')
-    print('opening raspi-config where the User needs to go to\n5 -> Camera Enable -> Enable -> back -> Finish')
-    os.system('sudo raspi-config')
-    print('Adding Camera Drivers')
-    os.system('sudo modprobe v4l2_common && sudo modprobe bcm2835-v4l2')
-    choice = input('Do you want to add the Camera into the Autostart? y = Yes, n = No ')
-
-    if choice == 'y':
-        os.system('echo "v4l2_common" | sudo tee -a /etc/modules && echo "bcm2835-v4l2" | sudo tee -a /etc/modules')
-    else:
-        print('Camera wasnt Added to Autostart')
-
-    os.system('ls /dev/video*')
+err5 = 'motion installation failed'
     
-    print('Camera successfully installed')
-    os.system('clear')
+def updateOS():
+    try:
+        print('upgrading Pi OS...')
+        os.system('sudo dpkg --configure -a')
+        # check for new updates and install when needed
+        os.system('sudo apt-get update -y && sudo apt-get upgrade -y')
+        os.system('clear')
+    except:
+        print(err0, sys.exc_info()[0])
 
-except:
-    print(err4, sys.exc_info()[0])
-    failcounter += 1
+
+def installi2c():
+    try:
+        print('installing i2c tools...')
+        os.system('sudo apt-get install i2c-tools -y')
+        os.system('sudo adduser pi i2c')
+        print('tesing if i2c is working')
+        os.system('sudo i2cdetect -y 1')
+        time.sleep(2)
+        os.system('clear')
+    except:
+        print(err1, sys.exc_info()[0])
 
 
-if failcounter > 2:
-    print('to many errors...............Exiting')
-    exit()
+def installSmBus():
+    try:
+        print('finished installing i2c tools continue...')
+        print('install smbus...')
+        os.system('sudo apt-get install python-smbus -y')
+        os.system('clear')
+    except:
+        print(err2, sys.exc_info()[0])
     
+
+def installWiringPi():
+    try:
+        print('installing wiringPi ...')
+        os.system('sudo apt-get install git git-core -y')
+        os.system('sudo pip install wiringpi')
+        os.system('clear')
+        print('wiringPi installation succesfull')
+    except:
+        print(err3, sys.exc_info()[0])  
+
+
+def installCamera():
+    try:
+        print('setup the Camera...')
+        print('opening raspi-config where the User needs to go to\n5 -> Camera Enable -> Enable -> back -> Finish')
+        os.system('sudo raspi-config')
+        print('Adding Camera Drivers')
+        os.system('sudo modprobe v4l2_common && sudo modprobe bcm2835-v4l2')
+        choice = input('Do you want to add the Camera into the Autostart? y = Yes, n = No ')
+
+        if choice == 'y':
+            os.system('echo "v4l2_common" | sudo tee -a /etc/modules && echo "bcm2835-v4l2" | sudo tee -a /etc/modules')
+        else:
+            print('Camera wasnt Added to Autostart')
+
+        os.system('ls /dev/video*')
+        print('Camera successfully installed')
+        os.system('clear')
+    except:
+        print(err4, sys.exc_info()[0])
+
+
+def installMotion():
+    try:
+        print('Install Motion...')
+        os.system('sudo apt-get install motion')
+    except:
+        print(err5, sys.exc_info()[0])
+
+
+def fullInstallation():
+    print('install everything')
+
+
+def main():
+    print('----------------CAMperPi Installation Menu----------------')
+    print('\n')
+    print('Welcome to the CAMperPi Main Menu, what would you like to do?')
+    print('1. Full Installation')
+    print('2. Update the RaspberryPi')
+    print('3. Install the I2c Modules')
+    print('4. Install the smbus Modules')
+    print('5. Install the wiringPi modules')
+    print('6. Install Motion')
+    print('7. Activate the Camera')
+    print('8. Exit')
+    menuChoice = int(input('\nChoose an Option by typing the number and press Enter'))
+    print('\n> ')
+    print('Your Choice: ', menuChoice)
+
+    if menuChoice == int(1):
+        fullInstallation()
+    elif menuChoice == int(2):
+        updateOS()
+    elif menuChoice == int(3):
+        installi2c
+    elif menuChoice == int(4):
+        installSmBus
+    elif menuChoice == int(5):
+        installWiringPi
+    elif menuChoice == int(6):
+        installMotion
+    elif menuChoice == int(7):
+        installCamera
+    elif menuChoice == int(8):
+        print('Thanks for using CAMperPi, we hope ur happy with our product')
+        print('Exit...')
+        time.sleep(5)
+        exit()
+
+main()
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import sys
 #import configparser
 
 #config = configparser.ConfigParser()
-setupCfg = []
+
 # functions
 
 def cleanup():
@@ -27,16 +27,17 @@ def setup():
         print('Starting Setup')
         pin = 18
         print('pin was set to', pin)
+        # set wiringpi to Mode GPIO
         wiringpi.wiringPiSetupGpio()
         wiringpi.pinMode(pin, wiringpi.GPIO.PWM_OUTPUT)
         wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
         print('GPIO, GPIO Mode and PWM Mode were set succesfully')
-
+        # set pwmClock and pwmRange
         wiringpi.pwmSetClock(512)
         wiringpi.pwmSetRange(2000)
         print('PWM clock and Range were set succesfully')
-
-        bus = smbus.SMBus(1)     # Erzeugen und Öffnen einer I2C-Instanz
+        # create and open of an I2C-Instanz
+        bus = smbus.SMBus(1)
         print('Bus pin set successfully')
         
         # if we would use a cfg file it would be delcared here which var would have which value
@@ -47,13 +48,11 @@ def setup():
         i2cAdr = 0x48           # I2C-Adresse des Wandlers
         poti =   0x00        # Poti
         
-        print('Setup finished succesfully continue in 10 secs')
+        print('Setup finished succesfully continue...')
         time.sleep(0.5)
-
         # return the cfg vars
         return pin, i2cAdr, poti, bus
         #servoMotorAuto(pin, i2cAdr, poti, bus)
-
     except KeyboardInterrupt:
         print('Setup failed, starting cleanup')
         cleanup()
@@ -85,11 +84,9 @@ def servoMotorAuto(pin, i2cAdr, potiAdr, bus):
 
 def servoMotorManual(pin, pwm):
     try:     
-        pwm = 30
-        print ('servo kacke: ', pwm)
+        print ('servo drehwinkel: ', pwm,' °')
         wiringpi.pwmWrite(pin, pwm)
         time.sleep(0.2)
-
     except KeyboardInterrupt:
         os.system('clear')
         print('\nUnexpcted Interruption from the user')
@@ -100,8 +97,12 @@ def servoMotorManual(pin, pwm):
     except:
         print('Unexpected error:', sys.exc_info()[0])
 
-def main(pin, i2cAdr, potiAdr, bus):
+
+def main():
+    # start the stetup
+    setup()
     # setup the vars for the wiring pi and following functions
+    setup()
     pin, i2cAdr, potiAdr, bus = setup()
     # execute automatic servo control over the tunring device
     servoMotorAuto(pin, i2cAdr, potiAdr, bus)
